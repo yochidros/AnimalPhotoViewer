5b912c5e7f38e2b8f5db48bc84ea17f489a32ed7
yochidros <mm9.movement.trb@gmail.com>
2019-07-06 12:16:58 +0900

    feat: image cache system.

--- AnimalPhotoViewer/Utils/ImageLoader.swift
+++ AnimalPhotoViewer/Utils/ImageLoader.swift
@@ -10,32 +10,40 @@ import Foundation
 import UIKit
 
 class ImageLoader: NSObject {
-  private static let shared: ImageLoader = ImageLoader()
-  
-  static func loadImage(url: String, into view: UIImageView?) {
-    guard let url = URL(string: url), let v = view else { return }
-    v.showLoading()
-    shared.load(url: url, view: v)
-  }
-  
-  private func load(url: URL, view: UIImageView) {
-    URLSession.shared.dataTask(with: url) { (data, _, error) in
-      if error != nil {
-        view.hideLoadingView()
-        return
-      }
-      
-      guard let data = data else {
-        view.hideLoadingView()
-        return
-      }
-      
-      DispatchQueue.main.async {
-        [view, data] in
-        view.image = UIImage(data: data)
-        view.hideLoadingView()
-      }
-    }.resume()
-  }
-
+	private static let shared: ImageLoader = ImageLoader()
+	
+	private let cached: NSCache<NSString, UIImage> = NSCache<NSString, UIImage>()
+	
+	static func loadImage(url: String, into view: UIImageView?) {
+		guard let url = URL(string: url), let v = view else { return }
+		v.showLoading()
+		shared.load(url: url, view: v)
+	}
+	
+	private func load(url: URL, view: UIImageView) {
+		if let cachedImage = cached.object(forKey: url.absoluteString as NSString) {
+			view.image = cachedImage
+			view.hideLoadingView()
+			return
+		}
+		URLSession.shared.dataTask(with: url) { [view, url, weak self] (data, _, error) in
+			if error != nil {
+				view.hideLoadingView()
+				return
+			}
+			
+			guard let data = data, let image = UIImage(data: data) else {
+				view.hideLoadingView()
+				return
+			}
+			self?.cached.setObject(image, forKey: url.absoluteString as NSString)
+			
+			DispatchQueue.main.async {
+				[view, image] in
+				view.image = image
+				view.hideLoadingView()
+			}
+			}.resume()
+	}
+	
 }
